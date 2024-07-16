9e00333ecfff8952a21922f5cafd8c01dd66d53f
sagarthoratv2 <sagar.thorat@v2solutions.com>
2024-07-16 16:03:18 +0530

    Modifying UI

--- src/ui/js/script.js
+++ src/ui/js/script.js
@@ -1,51 +1,30 @@
 const currentUrl = window.location.hostname;
-const apiUrl = `https://${currentUrl}/prod/deployment`;
+const apiUrl = 'https://' + currentUrl + '/prod/deployment';
 
-async function fetchData() {
-    console.log('Data:', apiUrl);
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST'
-        });
+function fetchData() {
+    fetch(apiUrl, {
+        method: 'POST'
+    })
+    .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        const data = await response.json();
-        console.log('Data:', data);
-        document.getElementById('outputTextbox').innerText = JSON.stringify(data, null, 2);
+        return response.json();
+    })
+    .then(data => {
+        displayOutput(data);
         showSuccessPrompt();
-    } catch (error) {
+    })
+    .catch(error => {
         console.error('Error fetching data:', error);
-        document.getElementById('outputTextbox').innerText = 'Error fetching data. Please try again later.';
-    }
+        displayOutput('Error fetching data. Please try again later.');
+    });
 }
 
-async function putData() {
+function putData() {
     const inputData = document.getElementById('myTextBox').value;
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            body: JSON.stringify({ Data: inputData, Type: 'PUT' }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log('Data:', data);
-        showSuccessPrompt();
-    } catch (error) {
-        console.error('Error pushing data:', error);
-        document.getElementById('outputTextbox').innerText = 'Error pushing data. Please try again later.';
-    }
-}
-
-function showSuccessPrompt() {
-    const promptElement = document.getElementById('successPrompt');
-    promptElement.style.display = 'block';
-    setTimeout(() => {
-        promptElement.style.display = 'none';
-    }, 3000);
-}
+    fetch(apiUrl, {
+        method: 'POST', 
+        body: JSON.stringify({ Data: inputData , Type : 'PUT' }),
+        headers: {
+            'Content-Type': 'application/json'
